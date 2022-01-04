abb0754aaeacef57b61e826cdee35f9667d0722e
boodscode237 <65740750+boodscode237@users.noreply.github.com>
2022-01-04 14:28:14 +0500

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string continueResult = "";
+            do
+            {
+                Console.Write("What is your first name: ");
+                string firstName = Console.ReadLine();
+
+                Console.WriteLine($"Hello {firstName}");
+                Console.Write("do you want to continue (yes/no): ");
+                continueResult = Console.ReadLine();
+
+            } while (continueResult.ToLower() == "yes");
+
+
+
+            Console.Write("What is your age: ");
+            string ageText = Console.ReadLine();
+            bool isValidAge = int.TryParse(ageText, out int age);
+
+            while (!isValidAge)
+            {
+                Console.WriteLine("That was not a valid age. Enter a valid age");
+                Console.Write("What is your age: ");
+                ageText = Console.ReadLine();
+                isValidAge = int.TryParse(ageText, out age);
+            }
+
+            Console.WriteLine($"Your age in 10 years will be {age += 10}");
+            Console.ReadLine();
+        }
+    }
+}
